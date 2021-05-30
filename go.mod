e73ef7a922213b3ec34913511527e8c0c3e70fb4
kangmingtay <kang.ming1996@gmail.com>
2021-05-31 01:49:31 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kangmingtay/learn-6.824.git
+
+go 1.16
--- src/main/mrsequential.go
+++ src/main/mrsequential.go
@@ -6,13 +6,16 @@ package main
 // go run mrsequential.go wc.so pg*.txt
 //
 
-import "fmt"
-import "../mr"
-import "plugin"
-import "os"
-import "log"
-import "io/ioutil"
-import "sort"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"plugin"
+	"sort"
+
+	"github.com/kangmingtay/learn-6.824.git/src/mr"
+)
 
 // for sorting by key.
 type ByKey []mr.KeyValue
